111189343dd059726ee3e5917186f33f9f4fbd20
Oscar González <osdagoso@hotmail.com>
2024-02-03 21:33:22 -0800

    Defined common types and dictionaries for character summary data

---
+++ src/data/Dictionaries.ts
@@ -0,0 +1,16 @@
+export const CampaignDictionary = new Map<number, string>([
+    [1, "Forgotten Realms"],
+    [2, "Eberron"],
+    [3, "Ravenloft"],
+    [4, "Dragonlance"]
+]);
+
+export const LifestyleDictionary = new Map<number, string>([
+    [0, "Wretched"],
+    [1, "Squalid"],
+    [2, "Poor"],
+    [10, "Modest"],
+    [20, "Comfortable"],
+    [40, "Wealthy"],
+    [100, "Aristocratic"]
+]);
---
+++ src/data/Types.ts
@@ -0,0 +1,15 @@
+export type Character = {
+    id: string;
+    name: string;
+    campaign: number;
+    lineage: string;
+    classes: string;
+    level: number;
+    background: string;
+    faction: string;
+    lifestyle: number;
+    backstory: string;
+    notes: string;
+    characterSheetLink: string;
+    imageURL: string;
+}
