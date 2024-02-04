9199589e8bea85635a236e504138f35915804afe
Oscar González <osdagoso@hotmail.com>
2024-02-04 05:02:57 -0800

    Added types for other entities + added dummy logic to inject calculated vals into Character

--- src/data/Types.ts
+++ src/data/Types.ts
@@ -4,11 +4,69 @@ export type Character = {
     campaign: number;
     lineage: string;
     classes: string;
+    level: number;
     background: string;
-    faction: string;
+    faction: string | null;
     lifestyle: number;
-    backstory: string;
-    notes: string;
-    characterSheetLink: string;
-    imageURL: string;
-}
+    gold: number;
+    downtime: number;
+    backstory: string | null;
+    notes: string | null;
+    characterSheetLink: string | null;
+    imageUrl: string | null;
+};
+
+export type MagicItem = {
+    id: string;
+    name: string;
+    flavorName: string | null;
+    itemType: number;
+    rarity: number;
+    isConsumable: boolean;
+    requiresAttunement: boolean;
+    description: string | null;
+    flavorDescription: string | null;
+    properties: string | null;
+    isEquipped: boolean;
+    characterId: string;
+    originLogId: string;
+    lossLogId: string;
+};
+
+export type StoryAward = {
+    id: string;
+    name: string;
+    description: string | null;
+    isActive: boolean;
+    characterId: string;
+    originLogId: string;
+};
+
+export type PlayerLog = {
+    id: string;
+    title: string;
+    timestamp: Date;
+    location: string;
+    dmName: string;
+    dmDci: string | null;
+    lengthHours: number;
+    gold: number;
+    downtime: number;
+    levels: number;
+    serviceHours: number;
+    traderCharacterId: string | null;
+    traderCharacterName: string | null;
+    traderOtherPlayer: string | null;
+    description: string | null;
+    characterId: string;
+};
+
+export type DMLog = {
+    id: string;
+    title: string;
+    timestamp: Date;
+    location: string;
+    lengthHours: number;
+    serviceHours: number;
+    description: string | null;
+};
--- src/hooks/useCharacter.ts
+++ src/hooks/useCharacter.ts
@@ -4,8 +4,9 @@ import { Character } from "../data/Types";
 function useCharacter(id: string) {
     const [character, setCharacter]= useState<Character | undefined>();
 
-    const loadCharacter = async(id: string) => {
+    const loadCharacter = async(calculatedValues: [number, number, number], id: string) => {
         return new Promise<Character>((resolve) => {
+            // TODO: Call to storage to fetch character data
             setTimeout(() => {
                 const character: Character = {
                     id: id,
@@ -13,21 +14,38 @@ function useCharacter(id: string) {
                     campaign: 1,
                     lineage: "Half-Orc",
                     classes: "Twilight Cleric of Selune 14 / Circle of Stars Druid 2",
+                    level: calculatedValues[0],
                     background: "Faction Agent",
                     faction: "Harpers",
                     lifestyle: 10,
-                    backstory: "",
-                    notes: "",
+                    gold: calculatedValues[1],
+                    downtime: calculatedValues[2],
+                    backstory: null,
+                    notes: null,
                     characterSheetLink: "https://ddb.ac/characters/78117532/2QPdLS",
-                    imageURL: ""
+                    imageUrl: null
                 }
                 resolve(character);
             }, 1000);
         });
     };
 
+    const calculateLevelsAndCurrencyForCharacter = async(id: string) => {
+        return new Promise<[number, number, number]>((resolve) => {
+            // TODO: Call to storage to fetch character logs and add all levels, gold and downtime - stored proc instead maybe?
+            setTimeout(() => {
+                const levels = 1 + 15;
+                const gold = 0 + 10000;
+                const downtime = 0 + 100;
+                resolve([levels, gold, downtime])
+            }, 1000);
+        });
+    };
+
     useEffect(() => {
-        loadCharacter(id).then(setCharacter);
+        calculateLevelsAndCurrencyForCharacter(id)
+            .then((calculatedValues) => loadCharacter(calculatedValues, id))
+            .then(setCharacter);
     }, []);
 
     return character;
