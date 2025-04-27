e503fa4748a42b4c6256d72503f70f2ddb291b37
Oscar González <osdagoso@hotmail.com>
2025-04-27 01:25:01 -0700

    Added API call to fetch all DM Logs

--- api/src/routes/dm-logs.ts
+++ api/src/routes/dm-logs.ts
@@ -3,16 +3,19 @@ import dmLogController from '../controllers/dm-logs';
 
 const router = express.Router({mergeParams: true});
 
-// POST route to create a character log
+// POST route to create a DM log
 router.post('/create', dmLogController.createDMLog);
 
-// POST route to update a character log
+// POST route to update a DM log
 router.post('/:id', dmLogController.updateDMLog);
 
-// GET route for a character log
+// GET route for all DM logs
+router.get('/', dmLogController.getDMLogs);
+
+// GET route for a DM log
 router.get('/:id', dmLogController.getDMLog);
 
-// DELETE route for a character log
+// DELETE route for a DM log
 router.delete('/:id', dmLogController.deleteDMLog);
 
 export default router;
