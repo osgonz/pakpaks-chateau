1bf03f23d70a51abce3676106f35fae9399bc302
Oscar González <osdagoso@hotmail.com>
2026-02-11 14:06:57 -0800

    Fixed potential bug where Protected Route logic would finish evaluating before app could fetch logged user info

--- client/src/components/shared/ProtectedRoutes.tsx
+++ client/src/components/shared/ProtectedRoutes.tsx
@@ -1,11 +1,20 @@
 import { Navigate, Outlet } from "react-router-dom";
+import LinearProgress from "@mui/material/LinearProgress";
 import { useAuth } from "./AuthContext";
 
 const ProtectedRoutes = () => {
     // Reference to logged in user
-    const { user } = useAuth();
+    const { isLoading, user } = useAuth();
 
-    return user ? <Outlet /> : <Navigate to="/" replace />;
+    return (
+        <>
+            { isLoading ? (
+                <LinearProgress />
+            ) : (
+                user ? <Outlet /> : <Navigate to="/" replace />
+            )}
+        </>
+    );
 };
 
 export default ProtectedRoutes;
