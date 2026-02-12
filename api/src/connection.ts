37db4bdce9f1736325653f150e9a1eb59099a8a3
Oscar González <osdagoso@hotmail.com>
2026-02-12 11:32:09 -0800

    Tweaked server api for Render deployment

--- api/src/connection.ts
+++ api/src/connection.ts
@@ -3,7 +3,7 @@ import mariadb from 'mariadb';
 
 // Create DB connection pool
 const pool = mariadb.createPool({
-    connectionLimit: 4,
+    connectionLimit: 3,
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
