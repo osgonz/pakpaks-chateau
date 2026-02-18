908420fa58a8b7d314205e8b2cba0332f020f5ce
Oscar González <osdagoso@hotmail.com>
2026-02-18 14:48:42 -0800

    Fixed bug where magic items showed twice in Character Log abstract if story awards were also earned

--- api/database/get_character_log_list.sql
+++ api/database/get_character_log_list.sql
@@ -13,26 +13,37 @@ BEGIN
             ELSE 0
         END AS isOwner
     FROM (
-        SELECT l.id,
-            l.type,
-            l.title,
-            l.timestamp,
-            l.location,
-            l.dmName,
-            l.dmDci,
-            l.lengthHours,
-            l.gold,
-            l.downtime,
-            l.levels,
-            l.serviceHours,
-            l.traderCharacterId,
-            l.traderCharacterName,
-            l.traderOtherPlayer,
-            l.description,
-            l.characterId,
-            GROUP_CONCAT(m.compoundName SEPARATOR ', ') AS magicItemNames,
-            GROUP_CONCAT(s.name SEPARATOR ', ') AS storyAwardNames
-        FROM characterlog l
+        SELECT ls.*,
+            GROUP_CONCAT(m.compoundName SEPARATOR ', ') AS magicItemNames
+        FROM (
+            SELECT l.id,
+                l.type,
+                l.title,
+                l.timestamp,
+                l.location,
+                l.dmName,
+                l.dmDci,
+                l.lengthHours,
+                l.gold,
+                l.downtime,
+                l.levels,
+                l.serviceHours,
+                l.traderCharacterId,
+                l.traderCharacterName,
+                l.traderOtherPlayer,
+                l.description,
+                l.characterId,
+                GROUP_CONCAT(s.name SEPARATOR ', ') AS storyAwardNames
+            FROM characterlog l
+            LEFT JOIN (
+                SELECT name,
+                originLogId
+                FROM storyaward
+                WHERE characterId = character_id
+            ) AS s ON s.originLogId = l.id
+            WHERE l.characterId = character_id
+            GROUP BY l.id
+        ) AS ls
         LEFT JOIN (
             SELECT CASE
             WHEN flavorName IS NULL
@@ -42,15 +53,8 @@ BEGIN
             originLogId
             FROM magicitem
             WHERE characterId = character_id
-        ) AS m ON m.originLogId = l.id
-        LEFT JOIN (
-            SELECT name,
-            originLogId
-            FROM storyaward
-            WHERE characterId = character_id
-        ) AS s ON s.originLogId = l.id
-        WHERE l.characterId = character_id
-        GROUP BY l.id
+        ) AS m ON m.originLogId = ls.id
+        GROUP BY ls.id
     ) AS lm
     JOIN `character` c ON c.id = lm.characterId
     LEFT JOIN (
